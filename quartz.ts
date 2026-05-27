9bba228e0b4e5abd0e43609ac0ca38cd379ffb4a
Zane601 <2947193496@qq.com>
2026-05-27 17:08:33 +0800

    Quartz sync: May 27, 2026, 5:08 PM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,46 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-const config = await loadQuartzConfig()
+const config = await loadQuartzConfig({
+	theme: {
+      fontOrigin: "local",
+      cdnCaching: true,
+      typography: {
+        header: {
+          name: "DM Serif Display",
+          weights: [400],
+        },
+        body: "Bricolage Grotesque",
+        code: "JetBrains Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#fffdfa",
+          lightgray: "#d1caba",
+          gray: "#9c9384",
+          darkgray: "#2A354B",
+          dark: "#08142C",
+          secondary: "#274B75",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#0c0f14",
+          lightgray: "#2b3443ff",
+          gray: "#5A657B",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7188A9",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+	}
+})
+
+
+
 export default config
 export const layout = await loadQuartzLayout()
+ 
